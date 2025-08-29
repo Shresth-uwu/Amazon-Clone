0ad730f04d0c96b1569dd9a960785c6e6c834d5d
ShresthUwU <shresthsinghal1284@gmail.com>
2025-08-29 13:37:18 +0530

    fixed the landing page error and cleaned the code a little bit

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
-import{cart, addToCart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -23,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
@@ -58,24 +60,22 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-
-
-function updateCartQuantity(){
+function updateCartQuantity() {
   let cartQuantity = 0;
 
-      cart.forEach((cartItemtem) => {
-        cartQuantity += cartItem.quantity;
-      });
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-      addToCart();
+      addToCart(productId);
       updateCartQuantity();
     });
   });
