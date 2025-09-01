b5b314986bf82d06c91bf2a29bec5be39c1fb9b5
ShresthUwU <shresthsinghal1284@gmail.com>
2025-09-01 18:41:33 +0530

    fixed error made final changes in checkout page, fixed errors and made corrected folder structure

--- data/deliveryoptions.js
+++ data/deliveryoptions.js
@@ -1,5 +1,4 @@
-export const deliveryoptions 
-= [{
+export const deliveryOptions = [{
   id: '1',
   deliveryDays: 7,
   priceCents: 0
@@ -8,7 +7,19 @@ export const deliveryoptions
   deliveryDays: 3,
   priceCents: 499
 }, {
-  id: '3', 
+  id: '3',
   deliveryDays: 1,
   priceCents: 999
-}]
+}];
+
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+}
